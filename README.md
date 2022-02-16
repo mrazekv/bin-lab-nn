828c883107643f6bbdcb6b6be000a359542f8b8b
Vojta Mrazek <mrazek@fit.vutbr.cz>
2022-02-16 09:28:07 +0100

    2022 draft

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
-# Biologií inspirované počítače
-Tento repozitář ukazuje práci s neuronovými sítěmi. Kompletní zadání úkolů hledejte na WIKI ve WISu.
+# Laboratorní cvičení BIN4 : Neuronové sítě
 
-Určeno pouze pro interní použití v kurzu FIT VUT v Brně.
+Cílem čtvrtého cvičení v předmětu BIN je seznámit se s neuronovými sítěmi a jejich výpočetní náročností.
+
+## Info o NN
+Projděte si teorii představenou na přednášce BIN. Dále se seznamte s prací s neuronovými sítěmi ve frameworku Tensorflow, která je popsaná v Jupyter noteboocích popisujících 
+
+* MLP sítě: https://github.com/mrazekv/bin-lab-nn/blob/master/mlp.ipynb
+* Konvoluční sítě: https://github.com/mrazekv/bin-lab-nn/blob/master/conv.ipynb
+
+## Vlastní spuštění a testování 
+Pro spuštění máte několik možností:
+
+* __DOPORUČENO__: stáhnout notebook na [Google CoLab](https://colab.research.google.com/notebook), můžete otevřít projekt přímo z Githubu (nutné zadat `mrazekv` do cesty, pak vybrat `bin-lab-nn` a příslušný notebook). Potom kliknete do boxu s kódem (kde můžete dělat změny) a pomocí Shift+Enter spustit daný blok. Pozor, je nutné postupovat postupně a nepřeskakovat kernely.
+
+* z repozitáře https://github.com/mrazekv/bin-lab-nn si stáhnout Python soubory. Tyto soubory můžete pustit u sebe (nutnost Python3 + Tensorflow + Keras a nejlépe aspoň základní GPU) či na serveru merlin (pozor, je potřeba spouštět příkazem `python3.8 <nazev_skriptu>`). 
+
+
+
+## Úkoly 
+Implementujte trénování a testování následujících sítí pro dataset MNIST a trénování pro __10 epoch__.
+
+Implementujte následující sítě:
+* MLP (fully-connected): 784-300-10
+* MLP (fully-connected): 784-100-10
+* MLP (fully-connected): 784-100-100-10
+* MLP (fully-connected): 784-300-300-10
+* Konvoluční 2 konvoluce + 120-84-10 fully connected
+* Konvoluční 2 konvoluce + 120-10 fully connected
+* Konvoluční 1 konvoluce + 120-84-10 fully connected (odstraňte druhou konvoluci a následnou pooling layer)
+* Konvoluční 1 konvoluce + 120-10 fully connected (odstraňte druhou konvoluci a následnou pooling layer)
+
+Vlastnosti sítí shrňte v __tabulce__, kde bude uveden:
+* Typ sítě
+* Dosažená validační přesnost (po __10 epochách__)
+* Počet násobení v plně propojených vrstvách
+* Počet násobení v konvolučních vrstvách
+* Počet trénovacích parametrů
+
+Vytvořte X-Y (scatter) __graf__, kde na ose X bude celkový počet násobení (~energie) a na ose Y bude výsledná přesnost. Diskutujte výsledky. Pro vykreslení můžete použít Excel, nebo rovnou můžete využít Python v Jupyter notebooku. Ukázka vykreslení je
+
+```py
+import matplotlib.pyplot as plt
+
+# Data ve formatu prikon / pocet_nasobeni
+# TODO: upravte dle vlastniho uvazeni
+data_mlp = [
+    (100, 10),
+    (200, 80),
+    (300, 60)
+]
+
+data_conv = [
+    (200, 70),
+    (250, 60)
+]
+plt.figure(figsize=(8, 6))
+
+plt.scatter(*zip(*data_mlp), color="tab:orange", label="MLP sítě")
+plt.scatter(*zip(*data_conv), color="tab:green", label="Konvoluční sítě")
+
+plt.xlim(0, None)
+plt.ylim(0, None)
+plt.xlabel("Příkon [počet násobení]")
+plt.ylabel("Přesnost [%]")
+plt.legend()
+
+# plt.savefig("obrazek.pdf")
+# plt.savefig("obrazek.png")
+plt.show()
+```
+
+
+Do výpočtu počtu násobení v plně propojených vrstvách je nutné zahrnout: počet vstupních neuronů a počet neuronů ve vrstvě. Pro výpočet počtu násobení v konvolučních vrstvách je nutné zahrnout: velikost vstupního obrázku, počet kanálů ve vstupním obrázku, velikost filtru, počet výstupních kanálů. 
+
+
+## Co si připravit pro hodnocení
+* Tabulka přesností + vzorec výpočtů)
+* Graf (včetně dodržení všech náležitostí grafu)
+* Shrnutí výsledků a závěr (ústně)
--- mlp.py
+++ mlp.py
@@ -2,14 +2,14 @@
 # coding: utf-8
 
 # # Neuronové sítě
-# V tomto cvičení se zaměříme na efektivitu neuronových sítí. Naším úkolem nebude jen se seznámit se základem NN (který možná máte z jiných kurzů), ale 
+# V tomto cvičení se zaměříme na efektivitu neuronových sítí. Naším úkolem nebude jen se seznámit se základem NN (který možná máte z jiných kurzů), ale zejména s jejich výpočetní náročností zejména při hardwarové implementaci.
 # 
 # 
 # ## Práce s neuronovou sítí
 # V prvním kroce musíme načíst požadované knihovny. Budeme pracovat s knihovnou [TensorFlow](https://tesorflow.org) a rozhraním Keras, které by mělo tvořit univerzální rozhraní pro práci s NN (i s jinými frameworky jako je např PyTorch). Toto rozhraní je v nové verzi TF 2.0 implicní, ale i ve starších je podporováno. Při načítání můžeme varování týkající se zastaralosti volání funkcí ``numpy`` ignorovat, jsou způsobeny využitím starší verze TensorFlow 1.14.
 # 
 
-# In[17]:
+# In[1]:
 
 
 import tensorflow.keras as keras
@@ -21,7 +21,7 @@ from tensorflow.keras.optimizers import RMSprop
 
 # Knihovny jsou úspěšně načtené. Dalším krokem bude stáhnout dataset MNIST obsahující ručně psaná čísla a který je základním benchmarkem pro NN. Tento dataset je (jako ostatní datasety) rozdělen na trénovací data a testovací data. 
 
-# In[18]:
+# In[2]:
 
 
 # the data, split between train and test sets
@@ -31,10 +31,23 @@ print(x_train.shape, x_test.shape)
 
 # Najdeme zde 60 tisíc testovacích a 10 tisíc testovacích obrázků 28x28 pixelů a k tomu odpovídající počet labelů (neboli odpovídajících čísel, které obrázky představují). Bude se tedy jednat o tzv. supervised learning (trénování s učitelem). Tento krátký příklad ukáže, jak data v datasetu vypadají. 
 
+# In[3]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(12, 4))
+for i in range(0, 10):
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(x_train[i, :, :].reshape(28, 28), cmap='gray', vmin=0, vmax=255)
+    plt.title(f"Label: {y_train[i]:d}")
+plt.tight_layout()
+
 
 # Nyní se můžeme dát do konstrukce sítě. Deklarujeme si parametry jako jsou počet batchů (dávek) a počet epoch, po které budeme trénovat
 
-# In[20]:
+# In[4]:
 
 
 batch_size = 128
@@ -43,7 +56,7 @@ epochs = 5
 
 # Trénovací data jsou reprezentovány jako byty (0 - 255). My je převedeme do formátu float čísel 0 - 1 a pro MLP síť překonvertujeme 28x28 obrázky do 1D pole o délce 784.
 
-# In[21]:
+# In[5]:
 
 
 x_train = x_train.reshape(60000, 784)
@@ -61,11 +74,12 @@ y_test = keras.utils.to_categorical(y_test, 10)
 
 
 # Data jsou připravena. Konečně se dostáváme ke konstrukci požadované sítě. Vytvoříme síť plně propojených vrstev: neuronů, kde každý vezme výstupy všech neuronů v předcházející vrstvě a vynásobí je vahou. 
-# ![mlp.png](mpl neuron)
+# 
+# ![mlp neuron](mlp.png)
 # 
 # V tomto případě použijeme síť 784-200-100-10 neuronů, kdy všechny kromě poslední budou používat ReLU aktivační funkce.
 
-# In[22]:
+# In[6]:
 
 
 model = Sequential()
@@ -78,7 +92,7 @@ model.summary()
 
 # Nyní můžeme model zkompilovat a spustit trénování. Tato operace může zabrat nějaký čas, zejména pokud nemáte GPU.
 
-# In[23]:
+# In[7]:
 
 
 model.compile(loss='categorical_crossentropy',
@@ -95,15 +109,26 @@ history = model.fit(x_train, y_train,
 
 # Model je nyní natrénovaný (na poměrně malý počet epoch). Nyní jej můžeme uložit nebo použít na jednom konkrétním obrázku. Výstupem je 10 hodnot, kdy každá říká pravděpodobnost toho, že obrázek spadá do určité kategorie. Na tomto příkladě můžeme vidět test čísla 9, kdy správně kategorie 9 má největší pravděpodobnost.
 
+# In[8]:
 
 
-# In[52]:
+img = x_test[16]
+est = model.predict(img.reshape(1, 784))
+
+plt.subplot(1, 2, 1)
+plt.imshow(img.reshape(28, 28), vmin=0, vmax=1)
+plt.subplot(1, 2, 2)
+plt.bar(range(0, 10), est.reshape(10))
+plt.xlabel("Odhadnuté číslo")
+plt.ylabel("Pravděpodobnost")
+plt.xticks(range(0, 10))
+plt.tight_layout()
+
+
+# In[9]:
 
 
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
-
-# 
-# 
